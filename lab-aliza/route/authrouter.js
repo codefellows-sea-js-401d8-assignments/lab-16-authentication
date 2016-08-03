8d476c8d3e4c02548448eff19214a65a01c652dc
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-03 00:37:03 -0700

    finishing for the night

--- lab-aliza/route/authrouter.js
+++ lab-aliza/route/authrouter.js
@@ -4,27 +4,23 @@ const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
 const User = require('../model/usermodel');
 const BasicHTTP = require('../lib/basichttp');
-const httpError = require('http-errors');
-
+const HTTPError = require('http-errors');
+const err400 = HTTPError(400, 'bad request');
+const err401 = HTTPError(401, 'unauthorized');
 let authRouter = Router();
 
 authRouter.post('/signup', jsonParser, (req, res, next) => {
-  let err400 = httpError(400, 'bad request');
   if (!req.body.username && !req.body.password) next(err400);
   let newUser = new User();
   newUser.basic.email = req.body.email;
   newUser.username = req.body.username || req.body.email;
   newUser.generateHash(req.body.password)
     .then((hash) => {
-      newUser.save((err, user) => {
-        if (err) return next(err400);
-        if (user) return res.json(hash);
-      });
+      newUser.save().then(res.json(hash), err400, next());
     });
 });
 
 authRouter.get('/signin', BasicHTTP, (req, res, next) => {
-  let err401 = httpError(401, 'unauthorized');
   if (!req.auth.username && !req.auth.password) next(err401);
   User.findOne({username: req.auth.username}, (err, user) => {
     if (err || !user) return next(err401);
