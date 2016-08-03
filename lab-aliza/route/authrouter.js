302ebb816a3db0ad922eff37aa4cca48c8d4dd4c
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-02 20:56:43 -0700

    fixed error handling

--- lab-aliza/route/authrouter.js
+++ lab-aliza/route/authrouter.js
@@ -2,28 +2,38 @@
 
 const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
-const ErrorHandler = require('../lib/errorhandler');
 const User = require('../model/usermodel');
 const BasicHTTP = require('../lib/basichttp');
+const httpError = require('http-errors');
 
 let authRouter = module.exports = exports = Router();
 
 authRouter.post('/signup', jsonParser, (req, res, next) => {
+  let err400 = httpError(400, 'bad request');
+  if (!req.body.username && !req.body.password) next(err400);
   let newUser = new User();
   newUser.basic.email = req.body.email;
   newUser.username = req.body.username || req.body.email;
   newUser.generateHash(req.body.password)
-    .then(() => {
-      newUser.save().then(res.json.bind(res), ErrorHandler(400, next));
-    }, ErrorHandler(500, next, 'Internal Server Error'));
+    .then((token) => {
+      newUser.save((err, user) => {
+        if (err) next(err400);
+        if (user) {
+          res.json(token);
+        }
+      });
+    }, err400);
 });
 
 authRouter.get('/signin', BasicHTTP, (req, res, next) => {
-  let authError = ErrorHandler(401, next, 'Authentication Error');
-  User.findOne({'basic.email': req.auth.username})
-    .then((user) => {
-      if (!user) return authError;
-      user.comparePassword(req.auth.password)
-        .then(res.json.bind(res), authError);
-    }, authError);
+  let authorizationErr = httpError(401, 'unauthorized');
+  if (!req.auth.username && !req.auth.password) next(authorizationErr);
+  User.findOne({username: req.auth.username}, (err, user) => {
+    if (err || !user) return next(authorizationErr);
+    user.comparePassword(req.auth.password)
+      .then(res.json.bind(res))
+      .catch((err) => {
+        next(err);
+      });
+  }, authorizationErr);
 });
