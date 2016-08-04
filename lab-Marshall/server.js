286455c1d8f6f9385708eda97e42062642880465
Mars <marshallmiller92@gmail.com>
2016-08-04 11:09:34 -0700

    changed 3000 log

--- lab-Marshall/server.js
+++ lab-Marshall/server.js
@@ -12,12 +12,12 @@ const authenticationRouter = require('./route/authenticationRouter');
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/auth_dev');
 
-app.use('/api', authorizationRouter);
-app.use('/api/signup', authenticationRouter);
+app.use('/api/authorization', authorizationRouter);
+app.use('/api/authentication', authenticationRouter);
 
 app.use((err, req, res, data) => {
   serverError(err);
   res.status(err.statusCode).json(err.message);
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('Server up on 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Server up!'));
