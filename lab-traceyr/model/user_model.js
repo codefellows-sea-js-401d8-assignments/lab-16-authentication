8a1f057e60de4c5df089c43ce45383971b9ecd6a
Tracey <radcliffe.tracey@gmail.com>
2016-08-03 21:58:33 -0700

    everything done except tests

--- lab-traceyr/model/user_model.js
+++ lab-traceyr/model/user_model.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 let userSchema = mongoose.Schema({
-  username: {type: String, required: true, unique: true},
+  username: String,
   basic: {
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true}
@@ -12,22 +12,23 @@ let userSchema = mongoose.Schema({
 });
 
 userSchema.methods.createHash = function(password){
-  new Promise((resolve, reject) =>{
+  return new Promise((resolve, reject) =>{
     bcrypt.hash(password, 8, (err, data) =>{
       if(err) return reject(err);
       this.basic.password = data;
-      resolve(data);
+      resolve({token: jwt.sign({idd: this.basic.email}, process.env.APP_SECRET)});
     });
   });
 };
 
 userSchema.methods.comparePass = function(password){
   return new Promise((resolve, reject) =>{
-    bcrypt.compare(password, this.basic.password, (err, data) =>{
+    bcrypt.hash(password, this.basic.password, (err, data) =>{
       if (err) return reject(err);
       if (data === false) return reject(new Error('No matching password'));
+      resolve({token: jwt.sign({idd: this.basic.email}, process.env.APP_SECRET)});
     });
   });
 };
 
-module.exports = exports = mongoose.model('Users', userSchema);
+module.exports = exports = mongoose.model('User', userSchema);
---
+++ lab-traceyr/route/auth_router.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const ErrorHandle = require('../lib/err_response');
+const User = require('../model/user_model');
+const BasicHTTP = require('../lib/http_handle');
+
+let authRouter = module.exports = exports = Router();
+
+authRouter.post('/signup', jsonParser, (req, res, next) =>{
+  let newUser = new User();
+  newUser.basic.email = req.body.email;
+  newUser.username = req.body.username || req.body.email;
+  newUser.createHash(req.body.password)
+    .then(() =>{
+      newUser.save().then(res.json.bind(res), ErrorHandle(400, next));
+    }, ErrorHandle(500, next, 'Server Error'));
+});
+
+authRouter.get('/signin', BasicHTTP, (req, res, next) =>{
+  User.findOne({'basic.email': req.auth.username})
+    .then((user)=>{
+      if(!user) return ErrorHandle(401, next, 'Could not Authorize');
+      user.comparePass(req.auth.password)
+        .then(res.json.bind(res), ErrorHandle(401, next, 'Could Not Authorize'));
+    }, ErrorHandle(401, next, 'Could Not Authorize'));
+});
