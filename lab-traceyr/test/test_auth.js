6b1473c44c10ba520bc172afd786bd94bb450491
Tracey <radcliffe.tracey@gmail.com>
2016-08-04 11:38:42 -0700

    finding error

---
+++ lab-traceyr/test/test_auth.js
@@ -0,0 +1,39 @@
+'use strict';
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const request = chai.request;
+const expect = chai.expect;
+const mongoose = require('mongoose');
+
+
+const TEST_DB_SERVER = 'mongodb://localhost/test_db';
+process.env.DB_SERVER = TEST_DB_SERVER;
+
+let app = require('./test_server');
+
+let server;
+
+describe('Authenication Testing', ()=>{
+  before((done)=>{
+    server = app.listen(4000, ()=>{
+      console.log('Server On 4000');
+      done();
+    });
+  });
+  after((done)=>{
+    mongoose.connection.db.dropDatabase(()=>{
+      server.close();
+      done();
+    });
+  });
+  it('POST should sign up a new user', (done)=>{
+    request('localhost:4000')
+      .post('/api/signup')
+      .send({email:'e@email.com', password:'words'})
+      .end((err, res)=>{
+        
+        done();
+      });
+  });
+});
